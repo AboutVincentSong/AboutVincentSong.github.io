4a0b8e28b9ef367a6130e9e0a63bdba07b1f8ee1
Vincent Song <dela.vincent.15@gmail.com>
2023-12-16 00:47:45 -0500

    Add links

--- app/page.tsx
+++ app/page.tsx
@@ -5,10 +5,16 @@ import 'bootstrap-icons/font/bootstrap-icons.css';
 
 export default function Home() {
   return (
-      <div className='navbar' style={{ padding: '22px'}}>
-        <a className="navbar-link" href="" style={{ fontSize: '20px', textDecoration: 'none', color: 'inherit'}}>
-          <i className="bi bi-code-square"></i> Vincent Song
-        </a>
+    <div className='navbar' style={{ paddingLeft: '50px', paddingRight: '50px', borderBottom: '1px solid black' }}>
+      <a className="navbar-link" href="" style={{ fontSize: '20px', textDecoration: 'none', color: 'inherit'}}>
+        <i className="bi bi-code-square"></i> Vincent Song
+      </a>
+      <div style={{ display: 'flex', justifyContent: 'center'}}>
+        <a className='navbar-link' style={{ textDecoration: 'none', color: 'inherit', paddingRight: '15px'}} href="">About</a>
+        <a className='navbar-link' style={{ textDecoration: 'none', color: 'inherit', paddingRight: '15px'}} href="">Work</a>
+        <a className='navbar-link' style={{ textDecoration: 'none', color: 'inherit', paddingRight: '15px'}} href="">Experience</a>
+        <a className='navbar-link' style={{ textDecoration: 'none', color: 'inherit'}} href="">Contact</a>
       </div>
+    </div>
   )
 }
