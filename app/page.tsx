d27548d12e72c4a07f5d944b67d64c1fe6583465
Vincent Song <dela.vincent.15@gmail.com>
2023-12-15 23:46:05 -0500

    Add logo

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,15 @@
 import Image from 'next/image'
 import './globals.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css'; 
 
 export default function Home() {
   return (
-      <body>Vincent Song</body>
+      <div className="container">
+        <br></br>
+        <a href="" style={{ fontSize: '20px', textDecoration: 'none', color: 'inherit', marginTop: '5px' }}>
+          <i className="bi bi-code-square"></i> Vincent Song
+        </a>
+      </div>
   )
 }
