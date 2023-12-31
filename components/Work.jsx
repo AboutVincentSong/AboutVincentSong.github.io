3da63f0ba34ce9994d33eeb85ecde1859e595f77
Vincent Song <dela.vincent.15@gmail.com>
2023-12-31 00:34:59 -0500

    Center latest projects

--- components/Work.jsx
+++ components/Work.jsx
@@ -54,14 +54,14 @@ const Work = () => {
   return (
     <section className="relative mb-12 xl:mb-48">
         <div className="container mx-auto">
-            <div className="max-w-[400px] mx-auto xl:mx-0 text-center xl:text-left mb-12 xl:h-[400px] flex flex-col justify-center items-center xl:items-start">
+            <div className="max-w-[600px] mx-auto text-center mb-12  flex flex-col justify-center items-center">
                 <h2 className="section-title mb-4"><ChevronRightCircle />Latest Projects</h2>
                 <p className="subtitle mb-8">These are my most recent projects from personal experience, school, work, and clubs. Each project emphasizes my capacity to innovate, address challenges, and excel through teamwork.</p>
                 <Link href='/projects'>
                     <Button>All projects</Button>
                 </Link>
             </div>
-            <div className="xl:max-w-[1000px] xl:absolute right-0 top-0">
+            <div className="xl:w-[1200px] mx-auto">
                 <Swiper 
                     className={`h-[480px] ${theme === 'dark' ? 'dark-slider-bullets' : ''}`}
                     slidesPerView={1} 
