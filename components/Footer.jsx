ee2a8fdd8f3bf79c1ad0021831d8027fae0d77ec
Vincent Song <dela.vincent.15@gmail.com>
2023-12-30 20:54:13 -0500

    Finish footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,6 +1,17 @@
+import Socials from "./Socials";
+
 const Footer = () => {
   return (
-    <footer>Footer</footer>
+    <footer className="bg-secondary py-8">
+      <div className="container mx-auto">
+        <div className="flex flex-col items-center justify-between">
+          <Socials containerStyles="flex gap-x-6 mx-auto xl:mx-0 mb-4" iconsStyles="text-primary text-[20px]" />
+          <div className="text-muted-foreground">
+            Copyright &copy; Vincent Song. All Rights Reserverd.
+          </div>
+        </div>
+      </div>
+    </footer>
   )
 }
 
