63a05f47550761085649d0dc4300ba7433ef2cd6
Vincent Song <dela.vincent.15@gmail.com>
2024-01-07 15:55:21 -0500

    Add form style

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -2,11 +2,14 @@
 import React, { useState } from 'react';
 import { ChevronRightCircle } from 'lucide-react';
 import axios from 'axios';
+import { useTheme } from 'next-themes';
+import { FileText, Send } from 'lucide-react';
 
 const Contact = () => {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [message, setMessage] = useState('');
+  const { theme } = useTheme();
 
   const handleSubmit = async (event) => {
     event.preventDefault();
@@ -25,6 +28,7 @@ const Contact = () => {
   };
 
   return (
+
     <section style={{ paddingTop: '70px' }} className='min-h-screen'>
       <div className="app__contact text-center mx-auto px-4 sm:px-6 lg:px-8 max-w-screen-sm lg:max-w-screen-sm xl:max-w-screen-sm">
         <h2 className="h2 mx-auto section-title text-align-center mb-8">
@@ -33,7 +37,7 @@ const Contact = () => {
 
         <form onSubmit={handleSubmit}>
           <div className="space-y-4">
-            <div className="p-4 rounded-md border border-gray-200 shadow-md">
+            <div className="p-4 rounded-md border dark:bg-[#1e293b] dark:border-gray-600 shadow-md">
               <input
                 type="text"
                 placeholder="Your Name"
@@ -41,9 +45,10 @@ const Contact = () => {
                 onChange={(e) => setName(e.target.value)}
                 required
                 className="w-full p-2 border-gray-300 rounded-md focus:outline-none focus:ring focus:border-blue-300"
+                style={{backgroundColor: theme === 'dark' ? '#1e293b' : ''}}
               />
             </div>
-            <div className="p-4 rounded-md border border-gray-200 shadow-md">
+            <div className="p-4 rounded-md border dark:bg-[#1e293b] dark:border-gray-600 shadow-md">
               <input
                 type="email"
                 placeholder="Email Address"
@@ -51,9 +56,10 @@ const Contact = () => {
                 onChange={(e) => setEmail(e.target.value)}
                 required
                 className="w-full p-2 border-gray-300 rounded-md focus:outline-none focus:ring focus:border-blue-300"
+                style={{backgroundColor: theme === 'dark' ? '#1e293b' : ''}}
               />
             </div>
-            <div className="p-4 rounded-md border border-gray-200 shadow-md">
+            <div className="p-4 rounded-md border dark:bg-[#1e293b] dark:border-gray-600 shadow-md">
               <textarea
                 placeholder="Message"
                 value={message}
@@ -61,14 +67,15 @@ const Contact = () => {
                 required
                 rows={4}
                 className="w-full p-2 border-gray-300 rounded-md focus:outline-none focus:ring focus:border-blue-300"
+                style={{backgroundColor: theme === 'dark' ? '#1e293b' : ''}}
               />
             </div>
             <div className="p-4">
               <button
                 type="submit"
-                className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded"
+                className="bg-blue-600 hover:bg-blue-500 text-white font-bold py-2 px-4 rounded-full flex items-center justify-center mx-auto"
               >
-                Submit
+                Send Message <Send size={18} className="ml-1"/>
               </button>
             </div>
           </div>
