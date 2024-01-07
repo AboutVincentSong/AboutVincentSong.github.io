8d4462bd4cb5c19aaeaeda57eb5df75c43a29166
Vincent Song <dela.vincent.15@gmail.com>
2024-01-07 17:04:46 -0500

    Add alert

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { ChevronRightCircle } from 'lucide-react';
 import axios from 'axios';
 import { useTheme } from 'next-themes';
@@ -9,20 +9,57 @@ const Contact = () => {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [message, setMessage] = useState('');
+  const [successMessage, setSuccessMessage] = useState('');
   const { theme } = useTheme();
 
-  return (
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
+    try {
+      await axios.post('https://api.web3forms.com/submit', {
+        access_key: '9d9b1321-3fd3-400d-9645-bbb54f209e36',
+        name,
+        email,
+        message,
+      });
+
+      setName('');
+      setEmail('');
+      setMessage('');
+      setSuccessMessage('Message sent successfully!');
+
+      setTimeout(() => {
+        setSuccessMessage('');
+      }, 15000);
+
+    } catch (error) {
+      console.error('Error submitting the form: ', error);
+    }
+  };
+
+  useEffect(() => {
+    setName('');
+    setEmail('');
+    setMessage('');
+  }, []);
+
+  return (
     <section style={{ paddingTop: '70px' }} className='min-h-screen'>
       <div className="app__contact text-center mx-auto px-4 sm:px-6 lg:px-8 max-w-screen-sm lg:max-w-screen-sm xl:max-w-screen-sm">
+        {successMessage && (
+          <div className="bg-green-200 text-green-800 rounded-md p-3 mb-4">
+            {successMessage}
+          </div>
+        )}
+
         <h2 className="h2 mx-auto section-title text-align-center mb-8">
           <ChevronRightCircle /> Contact Me
         </h2>
 
-        <form action="https://api.web3forms.com/submit" method="POST">
+        <form onSubmit={handleSubmit} action="https://api.web3forms.com/submit" method="POST">
           <input type="hidden" name="access_key" value="9d9b1321-3fd3-400d-9645-bbb54f209e36" />
           <div className="space-y-4">
-            <div className="p-4 rounded-md border dark:bg-[#1e293b] dark:border-gray-600 shadow-md">
+          <div className="p-4 rounded-md border dark:bg-[#1e293b] dark:border-gray-600 shadow-md">
               <input
                 type="text"
                 name="name"
