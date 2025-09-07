58cd80590f027392e3251880bc56d24635c7e6d6
Vincent Song <dela.vincent.15@gmail.com>
2025-09-06 23:44:57 -0400

    Update course names/numbers

--- components/Reviews.jsx
+++ components/Reviews.jsx
@@ -17,38 +17,38 @@ import { Pagination } from 'swiper/modules';
 const reviewsData = [
     {
         avatar: '/reviews/asd.png',
-        name: 'Advanced Software Development Techniques',
-        job: 'CS 3240',
+        name: 'Operating Systems',
+        job: 'CS 4414 / CS 6456',
         review: 'Topics Include: Agile Methodologies, Requirements Elicitation/Specification, Risk Management, Django, Heroku, DevOps, MVC Architecture, Usability, Personas/Wireframes, Verification/Validation, SE Code of Ethics, DEI, Security, Maintenance, and Licensing.'
     },
     {
         avatar: '/reviews/dsa2.png',
-        name: 'Data Structures and Algorithms 2',
-        job: 'CS 3100',
+        name: 'Database Systems',
+        job: 'CS 4750',
         review: 'Topics Include: Graphs, BFS/DFS, Topological Sorting, Dijkstra\'s Algorithm, Divide & Conquer Algorithms, Master Theorem, Greedy Algorithms, Minimum Spanning Trees, Huffman Coding, Dynamic Programming, Network Flow, Reductions, and Machine Learning Algorithms.'
     },
     {
         avatar: '/reviews/softwaretesting.png',
-        name: 'Software Testing',
-        job: 'CS 3250',
+        name: 'Internet Scale Applications',
+        job: 'CS 4260',
         review: 'Topics Include: Faults, Errors, Failures, RIPR Model, Model Driven Test Design (MDTD), Test Automation w/ JUnit 5 and Selenium; Input-Space Partition (ISP), Graph-based, Logic-based, Syntax-based, and Bypass Testing; Test-Driven Development (TDD).'
     },
     {
         avatar: '/reviews/cybersecurity.png',
-        name: 'Introduction to Cybersecurity',
-        job: 'CS 3710',
+        name: 'Machine Learning',
+        job: 'CS 4774 / CS 6316',
         review: 'Topics Include: Encryption, Networking, Web Security, Anonymity, SQL Injection Attacks, Cross Site Scripting (CSS), Cross-Site Request Forgery (CSRF) Cryptocurrency, Rootkits, Viruses, Buffer Overflows, Binary Exploits, Digital Forensics, and Virtual Machines.'
     },
     {
         avatar: '/reviews/sde.png',
-        name: 'Software Development Essentials',
-        job: 'CS 3140',
+        name: 'Natural Language Processing',
+        job: 'CS 6770',
         review: 'Topics Include: Java, Git, Build Tools (Gradle), Whitebox/Blackbox Testing (Mockito), Defensive Programming, Refactoring, Functional Programming, Integration, Software Design Principles, GUI, Design Patterns, JSON, Web-Services, SQLite, JDBC, Hibernate.'
     },
     {
         avatar: '/reviews/dsa1.png',
-        name: 'Data Structures and Algorithms 1',
-        job: 'CS 2100',
+        name: '2D + 3D Computer Vision',
+        job: 'CS 4501 / CS 6501',
         review: 'Topics Include: Interfaces, Polymorphism, Vectors, Java Generics, Linked Lists/Iterators, Stacks/Queues, Big-O Notation, Amortized Analysis, BST/AVL Trees, Insertion Sort, Mergesort, Quicksort, Hash Tables, Priority Queues/Binary Heap, and Concurrency/Threads.'
     },
 ]
